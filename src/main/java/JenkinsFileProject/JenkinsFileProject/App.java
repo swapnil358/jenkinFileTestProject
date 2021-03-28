dbb72f12fb751a21cf39ca01c2948ecc9971cd9e
swapnil358 <swapnil.bodade88@gmail.com>
2021-03-28 23:11:15 +0530

    first commit

---
+++ src/main/java/JenkinsFileProject/JenkinsFileProject/App.java
@@ -0,0 +1,13 @@
+package JenkinsFileProject.JenkinsFileProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
